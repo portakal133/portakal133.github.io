aa8ec49a0616ad5acf28136796253994c113bbde
portakal133 <98265522+portakal133@users.noreply.github.com>
2022-09-11 20:11:29 +0300

    Create xs.js

---
+++ xs.js
@@ -0,0 +1,24 @@
+
+function yenisekmedeac(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+   }
+   
+   function rastgelessac() {
+   yenisekmedeac(urlolustur());
+   }
+   
+   function urlolustur() {
+   return "https://prnt.sc/" + Id();
+   }
+   
+   function Id() {
+   var result = '';
+   var characters  = 'abcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < 6; i++ ) {
+   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+    return result;
+   }
+   
